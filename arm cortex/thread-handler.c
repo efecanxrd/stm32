c298f726cdb0d3b9297fbb340b0e0167cf78b0a7
Efecan Guler <s334666@studenti.polito.it>
2026-08-28 16:40:30 +0300

    Update thread-handler.c

--- arm cortex/thread-handler.c
+++ arm cortex/thread-handler.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 void RTC_WKUP_IRQHandler(void);
+void HardFault_Handler(void);
 
-void generate_interrupt() //Executes in THREAD MODE
+void generate_interrupt() //Executes in THREAD MODE (needs to be priviliged though)
 {
 	uint32_t *pSTIR = (uint32_t*) 0xE000EF00;
 	uint32_t *pISER0 = (uint32_t*) 0xE000E100;
@@ -12,9 +13,18 @@ void generate_interrupt() //Executes in THREAD MODE
 	*pSTIR = (3 & 0x1FF); //Generate an interrupt from software for IRQ3 - TRIGGERING
 }
 
+void change_access_level_unpriv(void) {
+	__asm volatile("MRS R0, CONTROL"); //read
+	__asm volatile("ORR R0,R0,#0x01"); //modify
+	__asm volatile("MSR CONTROL,R0"); //write
+}
+
 int main(void) //Executes in THREAD MODE
 {
 	printf("IN THREAD MODE: Before interrupt:\n");
+	void *(fun_ptr)(void);
+	fun_ptr = change_access_level_unpriv;
+	fun_ptr();
 	generate_interrupt();
 	printf("IN THREAD MODE: After interrupt\n");
 
@@ -23,4 +33,9 @@ int main(void) //Executes in THREAD MODE
 
 void RTC_WKUP_IRQHandler(void) { //Executes in HANDLER MODE
 	printf("In handler mode: ISR\n");
+}
+
+void HardFault_Handler(void) {
+	printf("Hard fault detected\n");
+	while(1);
 }
