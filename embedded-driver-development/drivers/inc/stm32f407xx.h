2a37e9af573114614c8a0e3d1515b6b113178f43
Efecan Guler <s334666@studenti.polito.it>
2026-09-20 13:24:28 +0300

    Update stm32f407xx.h

--- embedded-driver-development/drivers/inc/stm32f407xx.h
+++ embedded-driver-development/drivers/inc/stm32f407xx.h
@@ -224,6 +224,7 @@ typedef struct {
 #define GPIOH_REG_RESET()	do { (RCC->AHB1RSTR |= (1 << 7));	(RCC->AHB1RSTR &= ~(1 << 7)); } while(0)
 #define GPIOI_REG_RESET()	do { (RCC->AHB1RSTR |= (1 << 8));	(RCC->AHB1RSTR &= ~(1 << 8)); } while(0)
 
+#include "stm32f407xx_gpio_driver.h"
 
 #endif /* INC_STM32F407XX_H_ */
 
